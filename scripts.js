7bc0937e46f1d1c100ac96746390e38c9de2557f
Jessie Lesbian <jessielesbian@protonmail.com>
2022-03-12 17:47:37 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -361,10 +361,11 @@ let _main = async function(){
 						const data2 = [data[0]];
 						let prev = data[0];
 						const span = new BigInt(86400);
-						for(let i = 1; i < data.length && data2.length < 60; i++){
+						for(let i = 1; (i < data.length) && (data2.length < 60); i++){
 							const prevtime = prev.x;
 							const distance = parseInt((new BigInt(prevtime)).sub(new BigInt(data[i].x)).div(span).toString());
 							prev = data[i];
+							Console.log(distance);
 							for(let c = 0; c < distance; ){
 								data2.push({x: prevtime.sub(new BigInt(++c) * span).toString(), o: prev.c, h: prev.c, l: prev.c, c: prev.c});
 							}
