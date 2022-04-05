b7ed4ac9c031b908470ba57ddab1e544275897fa
WestnileOD <47566631+WestnileOD@users.noreply.github.com>
2022-04-04 23:15:46 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,54 +15,19 @@ const sidePanel = document.getElementById('app-sidepanel');
 const sidePanelDrop = document.getElementById('sidepanel-drop'); 
 const sidePanelClose = document.getElementById('sidepanel-close'); 
 
-window.addEventListener('load', function(){
-	responsiveSidePanel(); 
-});
+
 
 window.addEventListener('resize', function(){
 	responsiveSidePanel(); 
 });
 
 
-function responsiveSidePanel() {
-    let w = window.innerWidth;
-	if(w >= 1200) {
-	    // if larger 
-	    //console.log('larger');
-		sidePanel.classList.remove('sidepanel-hidden');
-		sidePanel.classList.add('sidepanel-visible');
-		
-	} else {
-	    // if smaller
-	    //console.log('smaller');
-	    sidePanel.classList.remove('sidepanel-visible');
-		sidePanel.classList.add('sidepanel-hidden');
-	}
-};
-
-sidePanelToggler.addEventListener('click', () => {
-	if (sidePanel.classList.contains('sidepanel-visible')) {
-		console.log('visible');
-		sidePanel.classList.remove('sidepanel-visible');
-		sidePanel.classList.add('sidepanel-hidden');
-		
-	} else {
-		console.log('hidden');
-		sidePanel.classList.remove('sidepanel-hidden');
-		sidePanel.classList.add('sidepanel-visible');
-	}
-});
 
 
 
-sidePanelClose.addEventListener('click', (e) => {
-	e.preventDefault();
-	sidePanelToggler.click();
-});
 
-sidePanelDrop.addEventListener('click', (e) => {
-	sidePanelToggler.click();
-});
+
+
 
 
 
